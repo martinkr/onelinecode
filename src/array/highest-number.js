f5f9ffb04ce3b24b232aa927bd439f66b8f00293
Martin Krause <github@mkrause.info>
2021-10-29 10:53:26 +0300

    feat(Array/highestNumber): Array, highest number

---
+++ src/array/highest-number.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the highest coerced numerical item of the array.
+ * @param {Array} arr 
+ * @returns {Number}
+ */
+const fn = arr => Math.max(...arr);
+
+export default fn;
---
+++ test/array/highest-number.spec.js
@@ -0,0 +1,41 @@
+import { biggestNumber } from '../../src/array';
+const test = require('ava');
+
+test('returns the biggest number of a collection of positive integers ', t => {
+    let input = [1, 2, 3];
+    let expectation = 3;
+    t.is(biggestNumber(input), expectation);
+});
+
+test('returns the biggest number of a collection of negative integers ', t => {
+    let input = [-1, -2, -3];
+    let expectation = -1;
+    t.is(biggestNumber(input), expectation);
+});
+
+test('returns the biggest number of a collection of positive and negative integers ', t => {
+    let input = [1, 0, -1];
+    let expectation = 1;
+    t.is(biggestNumber(input), expectation);
+});
+
+
+test('returns the biggest number of a collection of positive and negative floats', t => {
+    let input = [2.5, 1.5, -1.5, -2.5];
+    let expectation = 2.5;
+    t.is(biggestNumber(input), expectation);
+});
+
+test('returns the biggest number of a collection of positive and negative floats and integers', t => {
+    let input = [2.5, 1, 0, -1.5, -2];
+    let expectation = 2.5;
+    t.is(biggestNumber(input), expectation);
+});
+
+test('returns the number if there is only one number', t => {
+    let input = [1];
+    let expectation = 1;
+    t.is(biggestNumber(input), expectation);
+});
+
+
