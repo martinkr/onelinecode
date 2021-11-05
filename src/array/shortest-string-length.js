af954f7d4553844d13230447b624799c7268455f
Martin Krause <github@mkrause.info>
2021-11-05 12:50:54 +0200

    fix(Array/ShortestStringLength): performance

--- src/array/shortest-string-length.js
+++ src/array/shortest-string-length.js
@@ -1,8 +1,10 @@
 /**
  * Returns the length of the longest string entry of an Array of strings.
  * @param {Array} arr 
+ * @param {Number} arr 
  * @returns {Number}
  */
-const fn = arr => Math.min(...arr.map((el) => el.length));
+// const fn = arr => Math.min(...arr.map((el) => el.length));
+const fn = (arr, curr = Infinity) => (arr.forEach(el => { if (el.length < curr) curr = el.length }), curr);
 
 export default fn;
